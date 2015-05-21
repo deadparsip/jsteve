dbda86b22f1abb53364ba2b15399937aa8039687
jonStevens <jon.stevens@team.pottermore.com>
2015-05-21 17:21:26 +0100

    tidies - removing codes

--- js/jscript.js
+++ js/jscript.js
@@ -1,35 +1,48 @@
 'use strict';
 
+(function ($) {
+    $.fn.whelkit = function (text) {
+        return text.replace(/[^a-z,^A-Z]/g, '').replace('selected', '').replace('circle', '').slice(0, 20);
+    };
+}(jQuery));
+
+
+
 function Calippo() {
 
-	var loc = window.location.href.indexOf('poetry') > 0 ? 'poetry' : window.location.href.indexOf('stories') > 0 ? 'stories' : window.location.href.indexOf('films') > 0 ? 'films' : window.location.href.indexOf('news') > 0 ? 'news' : window.location.href.indexOf('pics') > 0 ? 'pics' : window.location.href.indexOf('blog') > 0 ? 'blog' : 'home',
-		$istouchdevice = typeof window.ontouchstart != 'undefined',
-        $ = jQuery,
-		$helper = $('.helper'),
-		$d = $(document),
-		$w = $(window),
-		$b = $('body'),
-		width = $w.width(),
-		height = $w.height(),
-		$next = $('a.next'),
+    var loc = window.location.href.indexOf('poetry') > 0 ? 'poetry' : 
+        window.location.href.indexOf('stories') > 0 ? 'stories' :
+        window.location.href.indexOf('films') > 0 ? 'films' : 
+        window.location.href.indexOf('news') > 0 ? 'news' : 
+        window.location.href.indexOf('pics') > 0 ? 'pics' : 
+        window.location.href.indexOf('blog') > 0 ? 'blog' : 'home',
+        
+        $istouchdevice = typeof window.ontouchstart !== 'undefined',
+        $helper = $('.helper'),
+        $d = $(document),
+        $b = $('body'),
+        $next = $('a.next'),
         $prev = $('a.prev'),
         $botty = $('#botty'),
         $boxes = $('.boxes'),
         timer = 1,
-		$nav = $('nav'),
-		_hash = window.location.hash,
-		cacheDate = localStorage.getItem('cache'),
-		cakes = {};    
+        $nav = $('nav'),
+		$go = $('p.go'),
+        _hash = window.location.hash,
+        cacheDate = localStorage.getItem('cache'),
+        cakes = {};
 
 
-		
+
     function nextItem(e) {
         window.location.hash = "";
         if (cakes.next('.boxes').length) {
             cakes.removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutLeftBig').on('animationend webkitAnimationEnd', function () {
                 window.location.hash = "";
                 cakes = $(this).next();
-                //window.localStorage.setItem('box' + loc, cakes.attr('class').split(" ")[0].replace('--', ''));
+                $('.selected').removeClass('selected');
+                $nav.find('.' + cakes.attr('class').split(" ")[0].replace('--', '')).addClass('selected');
+                //window.localStorage.setItem(`box  ${loc}`, cakes.attr('class').split(" ")[0].replace('--', ''));
                 $(this).hide().removeClass('fadeOutLeftBig').off('animationend webkitAnimationEnd')
                     .next('.boxes').show().addClass('fadeInRightBig');
                 if (!cakes.next('.boxes').length) {
@@ -39,15 +52,17 @@ function Calippo() {
             $prev.removeClass('opac');
         }
     }
-	
-	
+
+
 
     function prevItem(e) {
         window.location.hash = "";
         if (cakes.prev('.boxes').length) {
             cakes.removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutRightBig').on('animationend webkitAnimationEnd', function () {
                 cakes = $(this).prev();
-                //window.localStorage.setItem('box' + loc, cakes.attr('class').split(" ")[0].replace('--', ''));
+                $('.selected').removeClass('selected');
+                $nav.find('.' + cakes.attr('class').split(" ")[0].replace('--', '')).addClass('selected');
+                //window.localStorage.setItem(`box  ${loc}`, cakes.attr('class').split(" ")[0].replace('--', ''));
                 $(this).hide().removeClass('fadeOutRightBig').off('animationend webkitAnimationEnd')
                     .prev('.boxes').show().addClass('fadeInLeftBig');
                 if (!cakes.prev('.boxes').length) {
@@ -57,96 +72,61 @@ function Calippo() {
             $next.removeClass('opac');
         }
     }
-	
-	
+
+
 
     function getItem(e, item) {
         e.preventDefault();
         window.location.hash = "";
         $('.fadeInRightBig, .fadeInLeftBig').removeClass('fadeInLeftBig fadeInRightBig').addClass('fadeOutLeftBig').on('animationend webkitAnimationEnd', function () {
             cakes = $('.boxes.' + item);
-            //window.localStorage.setItem('box' + loc, item.replace('.boxes.', ''));
+            //window.localStorage.setItem(`box  ${loc}`, item.replace('.boxes.', ''));
             $(this).hide().removeClass('fadeOutLeftBig').off('animationend webkitAnimationEnd');
             cakes.show().addClass('fadeInLeftBig');
         });
     }
 
-
-
-    function getSizes() {
-        width = $w.width().toString();
-        var w = window.localStorage.getItem('widths');
-        if ((w !== null && w !== width) || w === null) {
-            window.localStorage.setItem('widths', width);
-            window.location.reload();
-
-        }
-    }
 	
-	
-	function init(loc) {
-		
-        $botty.on('mouseover touchstart', function () {
-            $(this).addClass("upIt");
-        }).on('mouseout', function () {
-            $(this).removeClass("upIt");
-        });
 
-        $b.css({ 'width': width, 'height': height });
-
-        /*if (cacheDate !== "trouserCouch") {
-            window.localStorage.clear();
-            window.localStorage.setItem('cache', "trouserCouch");
-        }
+    function init(loc) {
 
         if (_hash !== "" && _hash !== "#") {
-            localStorage.setItem('box' + loc, _hash.replace('#', '').toLowerCase());
-        }
-
-        var box = window.localStorage.getItem('box' + loc);
-        if (box !== null) {
-            cakes = $('.boxes.' + box).show().addClass('fadeInRightBig visible');
-            if ($('.boxes').eq(0).hasClass(box.toLowerCase())) {
-				$prev.removeClass('opac');
-			}
-            $('nav li.' + box).addClass('selected');
-        } else {*/
+            var aHash = _hash.replace('#', '').toLowerCase();
+            if ($('.boxes.' + aHash).length) {
+                cakes = $('.boxes.' + aHash).show().addClass('fadeInRightBig visible');
+                if ($boxes.eq(0).hasClass(aHash.toLowerCase()) && $prev.length) {
+                    $prev.removeClass('opac');
+                }
+                $nav.find('.'+aHash).addClass('selected');
+            } else {
+                cakes = $boxes.eq(0).show().addClass('fadeInRightBig');
+            }
+        } else {
             cakes = $('.boxes').eq(0).show().addClass('fadeInRightBig');
-            window.localStorage.setItem('box' + loc, cakes.attr('class').split(" ")[0].replace('--', ''));
-            //$('nav li:first').addClass('selected');
-        //}
+        }
 
         $next.on('click', nextItem);
         $prev.on('click', prevItem);
         $boxes.on("swiperight", prevItem);
         $boxes.on("swipeleft", nextItem);
 
-        $w.on('resize', function (event) {
-            clearTimeout(timer);
-            timer = setTimeout(getSizes, 200);
-        });
+        setTimeout(function () {
+            $helper.fadeOut(500);
+        }, 3000);
 
-
-		//$helper.show();
-		setTimeout(function () {
-			$helper.fadeOut(500);
-		}, 3000);
-
-
-        $($nav).on('click', function (event) {
+        $nav.on('click', function (event) {
             var link = ($(event.target).attr('class'));
-			if (link.indexOf('selected')>0) {
-				return
-			}
+            if (link.indexOf('selected') > 0) {
+                return
+            }
             getItem(event, link);
             $(event.target).addClass('selected').siblings().removeClass('selected');
         });
 
-        window.localStorage.setItem('widths', width);
 
-        $('p.go').click(function (e) {
+        $(document).on('click', '.go', function (e) {
             e.preventDefault();
-            $('body').addClass('fadeOutUpBig animated').on('animationend webkitAnimationEnd', function () {
+            $b.addClass('fadeOutUpBig animated').on('animationend webkitAnimationEnd', function () {
                 window.location.href = $(e.target).parents('a').attr('href');
             });
         });
